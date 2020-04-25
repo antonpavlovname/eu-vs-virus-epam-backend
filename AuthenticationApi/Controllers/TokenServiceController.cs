c9124b8787b1bdfb9754445a8d20d05b078bbdfc
Anton_Pavlov1 <sourceforge@antonpavlov.name>
2020-04-25 11:58:14 +0200

    Added mock authentication application

---
+++ AuthenticationApi/Controllers/TokenServiceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenServiceController : ControllerBase
+    {
+        // GET: api/TokenService/123456
+        [HttpGet("{token}", Name = "Get")]
+        public string Get(string token)
+        {
+            return "echo: "+ token;
+        }
+    }
+}
---
+++ AuthenticationApi/Controllers/UserServiceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserServiceController : ControllerBase
+    {
+        // GET: api/UserService/CZ123456?countryOfGovID=CZ
+        [HttpGet("{govID}", Name = "GetToken")]
+        public string GetToken(string govID, [FromQuery]string countryOfGovID)
+        {
+            return "mock-token: "+ govID + " : " + countryOfGovID;
+        }
+    }
+}
