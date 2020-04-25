f446d3d94ba3a53b28c19476a64c70631c3a9852
Anton_Pavlov1 <sourceforge@antonpavlov.name>
2020-04-25 16:24:35 +0200

    Added ReportDisease controller

---
+++ HealthPassportApi/Controllers/ReportDiseaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthPassportApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportDiseaseController : ControllerBase
+    {
+        [HttpPost("{uuid}/{disease}")]
+        public IActionResult Post(Guid uuid, string disease)
+        {
+            //TODO: Fire some complex logic
+            return Accepted();
+        }
+    }
+}
